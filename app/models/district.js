60cb74896cd3cc9f7101c5deb159303087cb8932
Jamie Burkart <jamie@wowlist.org>
2020-01-14 19:59:45 -0600

    add email templates

--- app/models/district.js
+++ app/models/district.js
@@ -48,4 +48,54 @@ export default Model.extend({
     return `https://twitter.com/intent/tweet?text=${encodeURIComponent(this.get('tweetText'))}&url=https://FairRentNYC.com&hashtags=FairRentNYC,StopDisplacement`;
   }),
 
+  sponsorEmailBody: computed('lastName', function () {
+    let message = `
+Council Member ${this.get('lastName')},
+
+Too many important neighborhood places are closing due to rent increases. We lose neighborhood culture.
+
+As a resident of your district, I thank you for sponsoring #FairRentNYC Commercial Rent Stabilization Bill #1796!
+
+Now I ask that you pass Commercial Rent Stabilization Bill #1796.
+
+This is very important to me, please let me know how you intend to speak with your colleagues about Commercial Rent Stabilization Bill #1796.
+
+More info: http://FairRentNYC.com
+
+Thank you`;
+    return message.trim();
+  }),
+
+  nonSponsorEmailBody: computed('lastName', function () {
+    let message = `
+Council Member ${this.get('lastName')},
+
+Too many important neighborhood places are closing due to rent increases. We lose neighborhood culture.
+
+As a resident of your district, I ask you to sponsor #FairRentNYC Commercial Rent Stabilization Bill #1796.
+
+This is very important to me, please let me know where you stand on Commercial Rent Stabilization Bill #1796.
+
+More info: FairRentNYC.com
+
+Thank you`;
+    return message.trim();
+  }),
+
+  emailUrl: computed('isSponsor', 'sponsorEmailBody', 'nonSponsorEmailBody', 'email', function () {
+    let email = this.get('email');
+    let subject = '';
+    let body = '';
+    if (this.get('isSponsor')) {
+      subject = 'Pass #FairRentNYC Commercial Rent Stabilization Bill #1796';
+      body = this.get('sponsorEmailBody');
+    } else {
+      subject = 'Sponsor #FairRentNYC Commercial Rent Stabilization Bill #1796';
+      body = this.get('nonSponsorEmailBody');
+    }
+
+    let url = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    return url;
+  }),
+
 });
