8b7bb128accc6c1312bff364890a45f853e2f19b
Jamie Burkart <jamie@wowlist.org>
2020-01-16 14:02:29 -0600

    https:// canonical link

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -177,8 +177,8 @@ export default Route.extend({
   project: 'Fair Rent NYC - Pass Commercial Rent Stabilization Bill 1796',
   description: 'Rent closes NYC spaces. Pass #FairRentNYC Commercial Rent Stabilization for diverse affordable neighborhoods.',
   // canonical: 'http://fairrentnyc.nycartc.com',
-  canonical: 'http://fairrentnyc.nycartc.com',
-  host: 'http://fairrentnyc.nycartc.com',
+  canonical: 'https://fairrentnyc.nycartc.com',
+  host: 'https://fairrentnyc.nycartc.com',
   image: '/s/img/fairrentnyc-og-image-10.jpg',
 
   headTags: Ember.computed('title', function () {
