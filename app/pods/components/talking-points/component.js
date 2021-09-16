1cf063315c056b2b45bed73d76ee6b0daf3c50a6
Jamie Burkart <jamie.burkart@gmail.com>
2021-09-15 22:41:41 -0500

    looking pretty good

--- app/pods/components/talking-points/component.js
+++ app/pods/components/talking-points/component.js
@@ -1,4 +1,8 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
+import { computed } from "@ember/object";
 
 export default Component.extend({
+  remainingCount: computed("sponsorCount", function() {
+    return 26 - this.get("sponsorCount");
+  })
 });
